d0ec2331d329a29acffe81f6c6443eb5934cea29
Jeff Tian <jeff.tian@outlook.com>
2023-08-21 19:43:52 +0800

    fix(UNI-160): add config property to weixin identity provider

--- src/main/java/org/keycloak/social/weixin/WeiXinIdentityProviderFactory.java
+++ src/main/java/org/keycloak/social/weixin/WeiXinIdentityProviderFactory.java
@@ -1,12 +1,17 @@
 package org.keycloak.social.weixin;
 
 import org.keycloak.broker.provider.AbstractIdentityProviderFactory;
-import org.keycloak.models.IdentityProviderModel;
 import org.keycloak.broker.social.SocialIdentityProviderFactory;
+import org.keycloak.models.IdentityProviderModel;
 import org.keycloak.models.KeycloakSession;
+import org.keycloak.provider.ConfiguredProvider;
+import org.keycloak.provider.ProviderConfigProperty;
+import org.keycloak.provider.ProviderConfigurationBuilder;
+
+import java.util.List;
 
 public class WeiXinIdentityProviderFactory extends AbstractIdentityProviderFactory<WeiXinIdentityProvider>
-        implements SocialIdentityProviderFactory<WeiXinIdentityProvider> {
+        implements SocialIdentityProviderFactory<WeiXinIdentityProvider>, ConfiguredProvider {
 
     public static final String PROVIDER_ID = "weixin";
 
@@ -30,5 +35,19 @@ public class WeiXinIdentityProviderFactory extends AbstractIdentityProviderFacto
         return PROVIDER_ID;
     }
 
+    @Override
+    public List<ProviderConfigProperty> getConfigProperties() {
+        return ProviderConfigurationBuilder.create()
+                .property().name(WeiXinIdentityProvider.CUSTOMIZED_LOGIN_URL_FOR_PC)
+                .label("PC端自定义登录URL")
+                .helpText("PC端自定义登录URL")
+                .type(ProviderConfigProperty.STRING_TYPE)
+                .add()
+                .build();
+    }
 
+    @Override
+    public String getHelpText() {
+        return "微信登录集成";
+    }
 }
