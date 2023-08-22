5deb5bbb2d901abf9436cb8d1ad0ed5022828c18
Jeff Tian <jeff.tian@outlook.com>
2023-08-22 19:57:11 +0800

    fix(UNI-160): add string type

--- src/main/java/org/keycloak/social/weixin/WeiXinIdentityProviderFactory.java
+++ src/main/java/org/keycloak/social/weixin/WeiXinIdentityProviderFactory.java
@@ -41,17 +41,19 @@ public class WeiXinIdentityProviderFactory extends
     public List<ProviderConfigProperty> getConfigProperties() {
         return ProviderConfigurationBuilder.create()
                 .property().name(WeiXinIdentityProvider.WECHAT_APPID_KEY)
-                .label("手机端微信 App Id")
-                .helpText("当用户使用手机进行微信登录时，要使用的 app Id")
+                .label("公众号 App Id")
+                .helpText("当用户使用 PC 进行关注微信公众号即登录时，要使用的 app Id，即微信公众号（不是开放平台）的 appid")
+                .type(ProviderConfigProperty.STRING_TYPE)
                 .add()
                 .property().name(WeiXinIdentityProvider.WECHAT_APPID_SECRET)
-                .label("手机端微信 App Secret")
-                .helpText("当用户使用手机进行微信登录时，要使用的 app Secret")
+                .label("公众号 App Secret")
+                .helpText("当用户使用 PC 进行关注微信公众号即登录时，要使用的 app Secret，即微信公众号（不是开放平台）的 app secret")
+                .type(ProviderConfigProperty.STRING_TYPE)
                 .add()
 
                 .property().name(WeiXinIdentityProvider.CUSTOMIZED_LOGIN_URL_FOR_PC)
                 .label("PC 登录 URL")
-                .helpText("PC 登录 URL 的登录页面，可以配置为一个自定义的前端登录页面")
+                .helpText("PC 登录 URL 的登录页面，可以配置为一个自定义的前端登录页面，用来展示公众号带参二维码")
                 .type(ProviderConfigProperty.STRING_TYPE)
                 .add().build();
     }
