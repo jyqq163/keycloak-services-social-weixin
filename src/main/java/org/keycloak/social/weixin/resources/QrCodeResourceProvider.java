6d25301588d209149b714948227c27949ee593e3
Jeff Tian <jeff.tian@outlook.com>
2023-09-04 20:04:54 +0800

    feat(#14): add resource provider in weixin identity provider

--- src/main/java/org/keycloak/social/weixin/resources/QrCodeResourceProvider.java
+++ src/main/java/org/keycloak/social/weixin/resources/QrCodeResourceProvider.java
@@ -1,12 +1,21 @@
 package org.keycloak.social.weixin.resources;
 
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import lombok.RequiredArgsConstructor;
+import org.jboss.logging.Logger;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.services.resource.RealmResourceProvider;
+
+import java.util.Map;
 
 @RequiredArgsConstructor
 public class QrCodeResourceProvider implements RealmResourceProvider {
     private final KeycloakSession session;
+    protected static final Logger logger = Logger.getLogger(QrCodeResourceProvider.class);
 
     @Override
     public Object getResource() {
@@ -16,5 +25,13 @@ public class QrCodeResourceProvider implements RealmResourceProvider {
     @Override
     public void close() {
 
+    }
+
+    @GET
+    @Path("hello")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response helloAnonymous() {
+        logger.info("hello");
+        return Response.ok(Map.of("hello", session.getContext().getRealm().getName())).build();
     }
 }
