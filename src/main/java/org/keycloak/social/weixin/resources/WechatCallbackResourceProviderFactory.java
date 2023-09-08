0c24cb17eec2c79b73db7e8eee7f1a8e447d7671
Jeff Tian <jeff.tian@outlook.com>
2023-09-08 19:09:07 +0800

    feat(#14): add wechat callback

---
+++ src/main/java/org/keycloak/social/weixin/resources/WechatCallbackResourceProviderFactory.java
@@ -0,0 +1,34 @@
+package org.keycloak.social.weixin.resources;
+
+import org.keycloak.Config;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+import org.keycloak.services.resource.RealmResourceProvider;
+import org.keycloak.services.resource.RealmResourceProviderFactory;
+
+public class WechatCallbackResourceProviderFactory implements RealmResourceProviderFactory {
+    @Override
+    public RealmResourceProvider create(KeycloakSession session) {
+        return new WechatCallbackResourceProvider(session);
+    }
+
+    @Override
+    public void init(Config.Scope config) {
+
+    }
+
+    @Override
+    public void postInit(KeycloakSessionFactory factory) {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public String getId() {
+        return "WechatCallbackResourceProviderFactory";
+    }
+}
